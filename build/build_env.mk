02f7d9027827868d44473c2bb70c389a5185bdfb
Víctor Guzmán <vguzman@fluendo.com>
2016-05-26 11:04:27 +0200

    Remove build_env.mk

--- build/build_env.mk
+++
@@ -1,8 +0,0 @@
-LM_DEPS = \
-	$(top_builddir)/bin/Couchbase.Lite.dll \
-	$(top_builddir)/bin/Newtonsoft.Json.dll \
-	$(top_builddir)/bin/Stateless.dll \
-	$(top_builddir)/bin/SQLitePCL.raw.dll \
-	$(top_builddir)/bin/SQLitePCL.ugly.dll \
-	$(top_builddir)/bin/ICSharpCode.SharpZipLib.Portable.dll \
-	$(top_builddir)/bin/PropertyChanged.dll
