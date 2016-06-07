54b67c1585ba0712372c98b38e5dba7ee95ea06a
Víctor Guzmán <vguzman@fluendo.com>
2016-06-07 16:19:50 +0200

    Use PlayerTick instead of TimeChanged

--- VAS.Drawing/Widgets/Timerule.cs
+++ VAS.Drawing/Widgets/Timerule.cs
@@ -135,12 +135,10 @@ namespace VAS.Drawing.Widgets
 			set {
 				if (player != null) {
 					player.PlaybackStateChangedEvent -= HandlePlaybackStateChanged;
-					player.TimeChangedEvent -= HandleTimeChangedEvent;
 				}
 				player = value;
 				if (player != null) {
 					player.PlaybackStateChangedEvent += HandlePlaybackStateChanged;
-					player.TimeChangedEvent += HandleTimeChangedEvent;
 				}
 			}
 		}
@@ -265,13 +263,6 @@ namespace VAS.Drawing.Widgets
 		void HandlePlaybackStateChanged (object sender, bool playing)
 		{
 			PlayingState = playing;
-		}
-
-		void HandleTimeChangedEvent (Time currentTime, Time duration, bool seekable)
-		{
-			CurrentTime = currentTime;
-			Duration = duration;
-			// FIXME: do we need to do anything with seekable?
 		}
 
 		public override void Draw (IContext context, Area area)
