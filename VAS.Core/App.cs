85281407226dc178c0a943d2637069e23dc6f0d4
dfernandez <dfernandez@fluendo.com>
2016-06-09 16:26:01 +0200

    Split Config.CS on VAS

---
+++ VAS.Core/App.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.IO;
+using VAS.Core;
+using VAS.Core.Common;
+using VAS.Core.Events;
+using VAS.Core.Interfaces;
+using VAS.Core.Interfaces.Drawing;
+using VAS.Core.Interfaces.GUI;
+using VAS.Core.Interfaces.Multimedia;
+
+namespace VAS
+{
+	public abstract class App
+	{
+		/* State */
+		public IGUIToolkit GUIToolkit;
+		public IMultimediaToolkit MultimediaToolkit;
+		public IDrawingToolkit DrawingToolkit;
+		public EventsBroker EventsBroker;
+
+		public IStorageManager DatabaseManager;
+		public IRenderingJobsManager RenderingJobsManger;
+
+
+		public string homeDirectory = ".";
+		public string baseDirectory = ".";
+		public string configDirectory = ".";
+		public string dataDir = ".";
+
+		/* State */
+		public EventsAggregator EventsAggregator;
+
+		protected StyleConf style;
+
+		public static App Current {
+			get;
+			set;
+		}
+
+		//		protected App (Config config)
+		//		{
+		//			Config = config;
+		//		}
+
+		public static void Init (App appInit, string evUninstalled, string softwareName, string portableFile, string evHome)
+		{
+			/* NOTE
+			*  All derived Configs should set the following:
+			*  
+			*  Config.baseDirectory
+			*  Config.configDirectory
+			*  Config.dataDir
+			*  Config.homeDirectory
+			*/
+
+			if (Current == null) {
+				return;
+			}
+
+			Current = appInit;
+			InitTranslations ();
+
+			string home = null;
+
+			if (Environment.GetEnvironmentVariable (evUninstalled) != null) {
+				App.Current.baseDirectory = Path.GetFullPath (".");
+				App.Current.dataDir = "../data";
+			} else {
+				if (Utils.OS == OperatingSystemID.Android) {
+					App.Current.baseDirectory = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+				} else if (Utils.OS == OperatingSystemID.iOS) {
+					App.Current.baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+				} else {
+					App.Current.baseDirectory = Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "../");
+					if (!Directory.Exists (Path.Combine (App.Current.baseDirectory, "share", softwareName))) {
+						App.Current.baseDirectory = Path.Combine (App.Current.baseDirectory, "../");
+					}
+				}
+				if (!Directory.Exists (Path.Combine (App.Current.baseDirectory, "share", softwareName)))
+					Log.Warning ("Prefix directory not found");
+				App.Current.dataDir = Path.Combine (App.Current.baseDirectory, "share", softwareName.ToLower ());
+			}
+
+			if (Utils.OS == OperatingSystemID.Android) {
+				home = App.Current.baseDirectory;
+			} else if (Utils.OS == OperatingSystemID.iOS) {
+				home = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "..", "Library");
+			} else {
+				/* Check for the magic file PORTABLE to check if it's a portable version
+					* and the config goes in the same folder as the binaries */
+				if (File.Exists (Path.Combine (App.Current.baseDirectory, portableFile))) {
+					home = App.Current.baseDirectory;
+				} else {
+					home = Environment.GetEnvironmentVariable (evHome);
+					if (home != null && !Directory.Exists (home)) {
+						try {
+							Directory.CreateDirectory (home);
+						} catch (Exception ex) {
+							Log.Exception (ex);
+							Log.Warning (String.Format (evHome + " {0} not found", home));
+							home = null;
+						}
+					}
+					if (home == null) {
+						home = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+					}
+				}
+			}
+
+			App.Current.homeDirectory = Path.Combine (home, softwareName);
+			App.Current.configDirectory = App.Current.homeDirectory;
+
+			// Migrate old config directory the home directory so that OS X users can easilly find
+			// log files and config files without having to access hidden folders
+			if (Environment.OSVersion.Platform != PlatformID.Win32NT) {
+				string oldHome = Path.Combine (home, "." + softwareName.ToLower ()); 
+				string configFilename = softwareName.ToLower () + "-1.0.config";
+				string configFilepath = Path.Combine (oldHome, configFilename);
+				if (File.Exists (configFilepath) && !File.Exists (App.Current.ConfigFile)) {
+					try {
+						File.Move (configFilepath, App.Current.ConfigFile);
+					} catch (Exception ex) {
+						Log.Exception (ex);
+					}
+				}
+			}
+		}
+
+		// copied from OneplayLongomMatch::CoreServices
+		static void InitTranslations ()
+		{
+			string localesDir = App.Current.RelativeToPrefix ("share/locale");
+
+			if (!Directory.Exists (localesDir)) {
+				var cerbero_prefix = Environment.GetEnvironmentVariable ("CERBERO_PREFIX");
+				if (cerbero_prefix != null) {
+					localesDir = Path.Combine (cerbero_prefix, "share", "locale");
+				} else {
+					Log.ErrorFormat ("'{0}' does not exist. This looks like an uninstalled execution." +
+					"Define CERBERO_PREFIX.", localesDir);
+				}
+			}
+			/* Init internationalization support */
+			Catalog.SetDomain (Constants.SOFTWARE_NAME.ToLower (), localesDir);
+		}
+
+		public Config Config {
+			get;
+			set;
+		}
+
+		public StyleConf Style {
+			get {
+				if (style == null) {
+					style = new StyleConf ();
+				}
+				return style;
+			}
+			set {
+				style = value;
+			}
+		}
+
+		public string ConfigFile {
+			get {
+				string filename = Constants.SOFTWARE_NAME.ToLower () + "-1.0.config";
+				return Path.Combine (App.Current.ConfigDir, filename);
+			}
+		}
+
+		public string HomeDir {
+			get {
+				return homeDirectory;
+			}
+		}
+
+		public string BaseDir {
+			set {
+				baseDirectory = value;
+			}
+		}
+
+		public string ConfigDir {
+			set {
+				configDirectory = value;
+			}
+			get {
+				return configDirectory;
+			}
+		}
+
+		public string TemplatesDir {
+			get {
+				return Path.Combine (DBDir, "templates");
+			}
+		}
+
+		public string PlayListDir {
+			get {
+				return Path.Combine (homeDirectory, "playlists");
+			}
+		}
+
+		public string SnapshotsDir {
+			get {
+				return Path.Combine (homeDirectory, "snapshots");
+			}
+		}
+
+		public string VideosDir {
+			get {
+				return Path.Combine (homeDirectory, "videos");
+			}
+		}
+
+		public string TempVideosDir {
+			get {
+				return Path.Combine (configDirectory, "temp");
+			}
+		}
+
+		public string DBDir {
+			get {
+				return Path.Combine (homeDirectory, "db");
+			}
+		}
+
+		public string RelativeToPrefix (string relativePath)
+		{
+			return Path.Combine (baseDirectory, relativePath);
+		}
+
+		#region Properties
+
+		public Image Background {
+			get;
+			set;
+		}
+
+		public string Copyright {
+			get;
+			set;
+		}
+
+		public string License {
+			get;
+			set;
+		}
+
+		public string SoftwareName {
+			get;
+			set;
+		}
+
+		public string SoftwareIconName {
+			get;
+			set;
+		}
+
+		public bool SupportsMultiCamera {
+			get;
+			set;
+		}
+
+		public bool SupportsFullHD {
+			get;
+			set;
+		}
+
+		public bool SupportsActionLinks {
+			get;
+			set;
+		}
+
+		public bool SupportsZoom {
+			get;
+			set;
+		}
+
+		public string LatestVersionURL {
+			get;
+			set;
+		}
+
+		public bool UseGameUnits {
+			get;
+			set;
+		}
+
+		public Version Version {
+			get;
+			set;
+		}
+
+		public string BuildVersion {
+			get;
+			set;
+		}
+
+		public Image FieldBackground {
+			get {
+				return Resources.LoadImage (Constants.FIELD_BACKGROUND);
+			}
+		}
+
+		public Image HalfFieldBackground {
+			get {
+				return Resources.LoadImage (Constants.HALF_FIELD_BACKGROUND);
+			}
+		}
+
+		public Image HHalfFieldBackground {
+			get {
+				return Resources.LoadImage (Constants.HHALF_FIELD_BACKGROUND);
+			}
+		}
+
+		public Image GoalBackground {
+			get {
+				return Resources.LoadImage (Constants.GOAL_BACKGROUND);
+			}
+		}
+
+		#endregion
+
+	}
+}
+
--- VAS.Core/Config.cs
+++ VAS.Core/Config.cs
@@ -16,9 +16,7 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 // 
 using System;
-using System.IO;
 using Newtonsoft.Json;
-using VAS.Core;
 using VAS.Core.Common;
 using VAS.Core.Events;
 using VAS.Core.Interfaces;
@@ -29,539 +27,145 @@ using VAS.Core.Serialization;
 
 namespace VAS
 {
-	public abstract class Config
+	[Serializable]
+	[JsonConverter (typeof(VASConverter))]
+	[PropertyChanged.ImplementPropertyChanged]
+	public abstract class Config : IChanged
 	{
-		/* State */
-		public static IGUIToolkit GUIToolkit;
-		public static IMultimediaToolkit MultimediaToolkit;
-		public static IDrawingToolkit DrawingToolkit;
-		public static EventsBroker EventsBroker;
-
-		public static IStorageManager DatabaseManager;
-		public static IRenderingJobsManager RenderingJobsManger;
-
-
-		public static string homeDirectory = ".";
-		public static string baseDirectory = ".";
-		public static string configDirectory = ".";
-		public static string dataDir = ".";
-		
-		/* State */
-		public static EventsAggregator EventsAggregator;
-
-		protected static StyleConf style;
-		protected static ConfigState state;
-
-		static ConfigState stateLongomatch {
-			get {
-				return (state as ConfigState);
-			}
-			set {
-				state = value;
-			}
-		}
-
-		public static void Init ()
-		{
-			/* NOTE
-			*  All derived Configs should set the following:
-			*  
-			*  Config.baseDirectory
-			*  Config.configDirectory
-			*  Config.dataDir
-			*  Config.homeDirectory
-			*/
-
-			InitTranslations ();
-		}
-
-		// copied from OneplayLongomMatch::CoreServices
-		public static void InitTranslations ()
-		{
-			string localesDir = Config.RelativeToPrefix ("share/locale");
-
-			if (!Directory.Exists (localesDir)) {
-				var cerbero_prefix = Environment.GetEnvironmentVariable ("CERBERO_PREFIX");
-				if (cerbero_prefix != null) {
-					localesDir = Path.Combine (cerbero_prefix, "share", "locale");
-				} else {
-					Log.ErrorFormat ("'{0}' does not exist. This looks like an uninstalled execution." +
-					"Define CERBERO_PREFIX.", localesDir);
-				}
-			}
-			/* Init internationalization support */
-			Catalog.SetDomain (Constants.SOFTWARE_NAME.ToLower (), localesDir);
-		}
-
-		public static void LoadState (ConfigState newState)
-		{
-			state = newState;
-		}
-
-		public static void Save ()
+		public void Save ()
 		{
 			try {
-				Serializer.Instance.Save (state, Config.ConfigFile);
+				Serializer.Instance.Save (this, App.Current.ConfigFile);
 			} catch (Exception ex) {
 				Log.Error ("Error saving config");
 				Log.Exception (ex);
 			}
 		}
 
-		public static StyleConf Style {
+		#region IChanged implementation
+
+		[JsonIgnore]
+		[PropertyChanged.DoNotNotify]
+		public bool IsChanged {
 			get {
-				if (style == null) {
-					style = new StyleConf ();
-				}
-				return style;
+				throw new NotImplementedException ();
 			}
 			set {
-				style = value;
-			}
-		}
-
-		public static string ConfigFile {
-			get {
-				string filename = Constants.SOFTWARE_NAME.ToLower () + "-1.0.config";
-				return Path.Combine (Config.ConfigDir, filename);
-			}
-		}
-
-		public static string HomeDir {
-			get {
-				return homeDirectory;
-			}
-		}
-
-		public static string BaseDir {
-			set {
-				baseDirectory = value;
-			}
-		}
-
-		public static string ConfigDir {
-			set {
-				configDirectory = value;
-			}
-			get {
-				return configDirectory;
-			}
-		}
-
-		public static string TemplatesDir {
-			get {
-				return Path.Combine (DBDir, "templates");
-			}
-		}
-
-		public static string PlayListDir {
-			get {
-				return Path.Combine (homeDirectory, "playlists");
-			}
-		}
-
-		public static string SnapshotsDir {
-			get {
-				return Path.Combine (homeDirectory, "snapshots");
-			}
-		}
-
-		public static string VideosDir {
-			get {
-				return Path.Combine (homeDirectory, "videos");
-			}
-		}
-
-		public static string TempVideosDir {
-			get {
-				return Path.Combine (configDirectory, "temp");
-			}
-		}
-
-		public static string DBDir {
-			get {
-				return Path.Combine (homeDirectory, "db");
-			}
-		}
-
-		public static string RelativeToPrefix (string relativePath)
-		{
-			return Path.Combine (baseDirectory, relativePath);
-		}
-
-		#region Properties
-
-		static public Image Background {
-			get;
-			set;
-		}
-
-		static public string Copyright {
-			get;
-			set;
-		}
-
-		static public string License {
-			get;
-			set;
-		}
-
-		static public string SoftwareName {
-			get;
-			set;
-		}
-
-		static public string SoftwareIconName {
-			get;
-			set;
-		}
-
-		static public bool SupportsMultiCamera {
-			get;
-			set;
-		}
-
-		static public bool SupportsFullHD {
-			get;
-			set;
-		}
-
-		static public bool SupportsActionLinks {
-			get;
-			set;
-		}
-
-		static public bool SupportsZoom {
-			get;
-			set;
-		}
-
-		static public string LatestVersionURL {
-			get;
-			set;
-		}
-
-		public static bool FastTagging {
-			get {
-				return state.fastTagging;
-			}
-			set {
-				state.fastTagging = value;
-				Save ();
-			}
-		}
-
-		public static bool UseGameUnits {
-			get;
-			set;
-		}
-
-		public static string Lang {
-			get {
-				return state.lang;
-			}
-			set {
-				state.lang = value;
-				Save ();
-			}
-		}
-
-		public static VideoStandard CaptureVideoStandard {
-			get {
-				return state.captureVideoStandard;
-			}
-			set {
-				state.captureVideoStandard = value;
-				Save ();
-			}
-		}
-
-		public static EncodingProfile CaptureEncodingProfile {
-			get {
-				return state.captureEncodingProfile;
-			}
-			set {
-				state.captureEncodingProfile = value;
-				Save ();
-
-			}
-		}
-
-		public static EncodingQuality CaptureEncodingQuality {
-			get {
-				return state.captureEncodingQuality;
-			}
-			set {
-				state.captureEncodingQuality = value;
-				Save ();
-
-			}
-		}
-
-		public static bool AutoSave {
-			get {
-				return state.autoSave;
-			}
-			set {
-				state.autoSave = value;
-				Save ();
-			}
-		}
-
-		public static bool AutoRenderPlaysInLive {
-			get {
-				return state.autorender;
-			}
-			set {
-				state.autorender = value;
-				Save ();
-			}
-		}
-
-		public static string AutoRenderDir {
-			get {
-				return state.autorenderDir;
-			}
-			set {
-				state.autorenderDir = value;
-				Save ();
-			}
-		}
-
-		public static string LastDir {
-			get {
-				return state.lastDir;
-			}
-			set {
-				state.lastDir = value;
-				Save ();
-			}
-		}
-
-		public static string LastRenderDir {
-			get {
-				return state.lastRenderDir;
-			}
-			set {
-				state.lastRenderDir = value;
-				Save ();
-			}
-		}
-
-		public static bool ReviewPlaysInSameWindow {
-			get {
-				return state.reviewPlaysInSameWindow;
-			}
-			set {
-				state.reviewPlaysInSameWindow = value;
-				Save ();
-			}
-		}
-
-		public static string DefaultTemplate {
-			get {
-				return state.defaultTemplate;
-			}
-			set {
-				state.defaultTemplate = value;
-				Save ();
-			}
-		}
-
-
-		public static ProjectSortMethod ProjectSortMethod {
-			get {
-				return state.projectSortMethod;
-			}
-			set {
-				state.projectSortMethod = value;
-				Save ();
-			}
-		}
-
-		public static Version IgnoreUpdaterVersion {
-			get {
-				return state.ignoreUpdaterVersion;
-			}
-			set {
-				state.ignoreUpdaterVersion = value;
-				Save ();
-			}
-		}
-
-
-
-		static public Version Version {
-			get;
-			set;
-		}
-
-		static public string BuildVersion {
-			get;
-			set;
-		}
-
-		static public Image FieldBackground {
-			get {
-				return Resources.LoadImage (Constants.FIELD_BACKGROUND);
-			}
-		}
-
-		static public Image HalfFieldBackground {
-			get {
-				return Resources.LoadImage (Constants.HALF_FIELD_BACKGROUND);
-			}
-		}
-
-		static public Image HHalfFieldBackground {
-			get {
-				return Resources.LoadImage (Constants.HHALF_FIELD_BACKGROUND);
-			}
-		}
-
-		static public Image GoalBackground {
-			get {
-				return Resources.LoadImage (Constants.GOAL_BACKGROUND);
-			}
-		}
-
-		public static string CurrentDatabase {
-			get {
-				return state.currentDatabase;
-			}
-			set {
-				state.currentDatabase = value;
-				Save ();
-			}
-		}
-
-		public static VideoStandard RenderVideoStandard {
-			get {
-				return state.renderVideoStandard;
-			}
-			set {
-				state.renderVideoStandard = value;
-				Save ();
-			}
-		}
-
-		public static EncodingProfile RenderEncodingProfile {
-			get {
-				return state.renderEncodingProfile;
-			}
-			set {
-				state.renderEncodingProfile = value;
-				Save ();
-		
-			}
-		}
-
-		public static EncodingQuality RenderEncodingQuality {
-			get {
-				return state.renderEncodingQuality;
-			}
-			set {
-				state.renderEncodingQuality = value;
-				Save ();
-			}
-		}
-
-		public static bool OverlayTitle {
-			get {
-				return state.overlayTitle;
-			}
-			set {
-				state.overlayTitle = value;
-				Save ();
-			}
-		}
-
-		public static bool EnableAudio {
-			get {
-				return state.enableAudio;
-			}
-			set {
-				state.enableAudio = value;
-				Save ();
-			}
-		}
-
-		public static uint FPS_N {
-			get {
-				return state.fps_n;
-			}
-			set {
-				state.fps_n = value;
-				Save ();
-			}
-		}
-
-		public static uint FPS_D {
-			get {
-				return state.fps_d;
-			}
-			set {
-				state.fps_d = value;
 				Save ();
 			}
 		}
 
 		#endregion
 
-	}
+		public string Lang {
+			get;
+			set;
+		} = null;
 
-	[Serializable]
-	[JsonConverter (typeof(VASConverter))]
-	public class ConfigState
-	{
-		public bool fastTagging;
-		public bool autoSave;
-		public string currentDatabase;
-		public string lang;
-		public uint fps_n;
-		public uint fps_d;
-		public VideoStandard captureVideoStandard;
-		public VideoStandard renderVideoStandard;
-		public EncodingProfile captureEncodingProfile;
-		public EncodingProfile renderEncodingProfile;
-		public EncodingQuality captureEncodingQuality;
-		public EncodingQuality renderEncodingQuality;
-		public bool overlayTitle;
-		public bool enableAudio;
-		public bool autorender;
-		public string autorenderDir;
-		public string lastRenderDir;
-		public string lastDir;
-		public bool reviewPlaysInSameWindow;
-		public string defaultTemplate;
-		public ProjectSortMethod projectSortMethod;
-		public Version ignoreUpdaterVersion;
+		public VideoStandard CaptureVideoStandard {
+			get;
+			set;
+		} = VideoStandards.P480_16_9.Clone ();
 
-		public ConfigState ()
-		{
-			/* Set default values */
-			fastTagging = false;
-			currentDatabase = Constants.DEFAULT_DB_NAME;
-			lang = null;
-			autoSave = false;
-			captureVideoStandard = VideoStandards.P480_16_9.Clone ();
-			captureEncodingProfile = EncodingProfiles.MP4.Clone ();
-			captureEncodingQuality = EncodingQualities.Medium.Clone ();
-			renderVideoStandard = VideoStandards.P720_16_9.Clone ();
-			renderEncodingProfile = EncodingProfiles.MP4.Clone ();
-			renderEncodingQuality = EncodingQualities.High.Clone ();
-			overlayTitle = true;
-			enableAudio = false;
-			fps_n = 25;
-			fps_d = 1;
-			autorender = false;
-			autorenderDir = null;
-			lastRenderDir = null;
-			lastDir = null;
-			reviewPlaysInSameWindow = true;
-			defaultTemplate = null;
-			projectSortMethod = ProjectSortMethod.Date;
-			ignoreUpdaterVersion = null;
-		}
+		public EncodingProfile CaptureEncodingProfile {
+			get;
+			set;
+		} = EncodingProfiles.MP4.Clone ();
+
+		public EncodingQuality CaptureEncodingQuality {
+			get;
+			set;
+		} = EncodingQualities.Medium.Clone ();
+
+		public bool AutoSave {
+			get;
+			set;
+		} = false;
+
+		public bool AutoRenderPlaysInLive {
+			get;
+			set;
+		} = false;
+
+		public string AutoRenderDir {
+			get;
+			set;
+		} = null;
+
+		public string LastDir {
+			get;
+			set;
+		} = null;
+
+		public string LastRenderDir {
+			get;
+			set;
+		} = null;
+
+		public bool ReviewPlaysInSameWindow {
+			get;
+			set;
+		} = true;
+
+		public string DefaultTemplate {
+			get;
+			set;
+		} = null;
+
+
+		public ProjectSortMethod ProjectSortMethod {
+			get;
+			set;
+		} = ProjectSortMethod.Date;
+
+		public Version IgnoreUpdaterVersion {
+			get;
+			set;
+		} = null;
+
+		public VideoStandard RenderVideoStandard {
+			get;
+			set;
+		} = VideoStandards.P720_16_9.Clone ();
+
+		public EncodingProfile RenderEncodingProfile {
+			get;
+			set;
+		} = EncodingProfiles.MP4.Clone ();
+
+		public EncodingQuality RenderEncodingQuality {
+			get;
+			set;
+		} = EncodingQualities.High.Clone ();
+
+		public bool OverlayTitle {
+			get;
+			set;
+		} = true;
+
+		public bool EnableAudio {
+			get;
+			set;
+		} = false;
+
+		public uint FPS_N {
+			get;
+			set;
+		} = 25;
+
+		public uint FPS_D {
+			get;
+			set;
+		} = 1;
+
+		public bool FastTagging {
+			get;
+			set;
+		} = false;
+
+		public string CurrentDatabase {
+			get;
+			set;
+		} = Constants.DEFAULT_DB_NAME;
 	}
 }
