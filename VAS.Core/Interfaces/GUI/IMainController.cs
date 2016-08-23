90b6015d881a4d3ee613115196070f71507220cb
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-08-23 10:18:55 +0200

    Remove AddExportEntry from the IMainController interface
    Export plugins are now registered in a different way

--- VAS.Core/Interfaces/GUI/IMainController.cs
+++ VAS.Core/Interfaces/GUI/IMainController.cs
@@ -22,10 +22,6 @@ namespace VAS.Core.Interfaces.GUI
 {
 	public interface IMainController
 	{
-		// FIXME: This has to disappear when Longomatch is adapted to use ITools.
-		[Obsolete]
-		void AddExportEntry (string name, string shortName, Action<Project, IGUIToolkit> exportAction);
-
 		bool SetPanel (IPanel newPanel);
 	}
 }
