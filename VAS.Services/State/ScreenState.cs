882418349836c8f0c43665b300f192bed356313a
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-09-12 19:01:45 +0200

    Add a base class for screen states

---
+++ VAS.Services/State/ScreenState.cs
@@ -0,0 +1,103 @@
+﻿//
+//  Copyright (C) 2016 Andoni Morales Alastruey.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VAS.Core;
+using VAS.Core.Hotkeys;
+using VAS.Core.Interfaces.GUI;
+using VAS.Core.Interfaces.MVVMC;
+
+
+namespace VAS.Services.State
+{
+	/// <summary>
+	/// Generic base class for <see cref="IScreenState"/> that takes care of the <see cref="IPanel"/>
+	/// <see cref="IViewModel"/> and <see cref="IController"/> creationg and wireup.
+	/// </summary>
+	public abstract class ScreenState<TViewModel> : IScreenState where TViewModel : IViewModel
+	{
+
+		public ScreenState ()
+		{
+			Panel = App.Current.ViewLocator.Retrieve (Name) as IPanel;
+			Controllers = App.Current.ControllerLocator.RetrieveAll (Name);
+			KeyContext = new KeyContext ();
+			foreach (IController controller in Controllers) {
+				KeyContext.KeyActions.AddRange (controller.GetDefaultKeyActions ());
+			}
+			KeyContext panelKeyContext = Panel.GetKeyContext ();
+			if (panelKeyContext != null) {
+				KeyContext.KeyActions.AddRange (Panel.GetKeyContext ()?.KeyActions);
+			}
+		}
+
+		public abstract string Name {
+			get;
+		}
+
+		public List<IController> Controllers {
+			get;
+			set;
+		}
+
+		public IPanel Panel {
+			get;
+			set;
+		}
+
+		public KeyContext KeyContext {
+			get;
+			set;
+		}
+
+		public TViewModel ViewModel {
+			get;
+			protected set;
+		}
+
+		public virtual Task<bool> PostTransition ()
+		{
+			foreach (IController controller in Controllers) {
+				controller.Stop ();
+			}
+			ViewModel = default (TViewModel);
+			App.Current.KeyContextManager.RemoveContext (KeyContext);
+			return AsyncHelpers.Return (true);
+		}
+
+		public Task<bool> PreTransition (dynamic data)
+		{
+			CreateViewModel (data);
+			Panel.SetViewModel (ViewModel);
+			foreach (IController controller in Controllers) {
+				controller.SetViewModel (ViewModel);
+				controller.Start ();
+			}
+			App.Current.KeyContextManager.AddContext (KeyContext);
+			return AsyncHelpers.Return (true);
+		}
+
+		/// <summary>
+		/// Creates the view model and fill using if needed the data passed as arguments.
+		/// </summary>
+		/// <param name="data">Data.</param>
+		protected abstract void CreateViewModel (dynamic data);
+	}
+}
+
