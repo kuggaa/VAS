2652186c7fcbd4fcf4279c224be83acab094fe3f
aoller <aoller@fluendo.com>
2016-10-24 13:58:46 +0200

    Move Registry Services to VAS

---
+++ VAS.Core/MVVMC/RegistryAttribute.cs
@@ -0,0 +1,32 @@
+﻿//
+//  Copyright (C) 2016 Fluendo S.A.
+//
+//
+using System;
+namespace VAS.Core.MVVMC
+{
+	/// <summary>
+	/// Attribute used to register Services components.
+	/// </summary>
+	[AttributeUsage (AttributeTargets.Class, AllowMultiple = false)]
+	public class RegistryAttribute : Attribute
+	{
+		public RegistryAttribute (Type interfaceType, int priority)
+		{
+			InterfaceType = interfaceType;
+			Priority = priority;
+		}
+
+		/// <summary>
+		/// Gets or sets the type of the interface implemented.
+		/// </summary>
+		/// <value>The type of the interface implemented.</value>
+		public Type InterfaceType { get; set; }
+
+		/// <summary>
+		/// Gets or sets the priority.
+		/// </summary>
+		/// <value>The priority.</value>
+		public int Priority { get; set; }
+	}
+}
---
+++ VAS.Core/MVVMC/RegistryScanner.cs
@@ -0,0 +1,23 @@
+﻿//
+//  Copyright (C) 2016 Fluendo S.A.
+//
+//
+using System;
+using System.Reflection;
+
+namespace VAS.Core.MVVMC
+{
+	public static class RegistryScanner
+	{
+		public static void Scan ()
+		{
+			Assembly assembly = Assembly.GetCallingAssembly ();
+			foreach (Type type in assembly.GetTypes ()) {
+				foreach (var attribute in type.GetCustomAttributes (typeof (RegistryAttribute), true)) {
+					var regAttribute = (attribute as RegistryAttribute);
+					App.Current.DependencyRegistry.Register (regAttribute.InterfaceType, type, regAttribute.Priority);
+				}
+			}
+		}
+	}
+}
