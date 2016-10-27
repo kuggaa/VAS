804beb5cc111ea9f6f6758cc9ee365f94bcf1ec1
Víctor Guzmán <vguzman@fluendo.com>
2016-10-27 15:51:37 +0200

    Import HockeySDK Core.45

---
+++ HockeySDK-Windows/Src/Kit.Core45/Services/IUnhandledExceptionTelemetryModule.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.HockeyApp.Services
+{
+    using Channel;
+    using DataContracts;
+    using Extensibility;
+    using System;
+
+    internal interface IUnhandledExceptionTelemetryModule : ITelemetryModule
+    {
+        ITelemetry CreateCrashTelemetry(Exception exception, ExceptionHandledAt handledAt);
+    }
+}
