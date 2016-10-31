8aff57e28b15925d2e24319c3dcb1263bf008ea7
Víctor Guzmán <vguzman@fluendo.com>
2016-10-31 16:54:46 +0100

    Move call to CreateFilterAndSort to TreeViewBase

--- VAS.UI.Gtk2/UI/Common/TreeViewBase.cs
+++ VAS.UI.Gtk2/UI/Common/TreeViewBase.cs
@@ -68,6 +68,7 @@ namespace VAS.UI.Common
 		public virtual void SetViewModel (object viewModel)
 		{
 			this.ViewModel = viewModel as TCollectionViewModel;
+			CreateFilterAndSort ();
 		}
 
 		public TCollectionViewModel ViewModel {
@@ -122,7 +123,7 @@ namespace VAS.UI.Common
 				break;
 			}
 
-			filter.Refilter ();
+			filter?.Refilter ();
 		}
 
 		protected virtual void AddSubViewModel (IViewModel subViewModel, TreeIter parent)
@@ -218,7 +219,7 @@ namespace VAS.UI.Common
 			}
 			TreeIter iter = dictionaryStore [senderVM];
 			Model.EmitRowChanged (store.GetPath (iter), iter);
-			filter.Refilter ();
+			filter?.Refilter ();
 			this.QueueDraw ();
 		}
 
@@ -279,7 +280,7 @@ namespace VAS.UI.Common
 				}
 			}
 			ViewModel.SelectionReplace (selected);
-			filter.Refilter ();
+			filter?.Refilter ();
 		}
 
 		protected virtual void HandleViewModelPropertyChanged (object sender, PropertyChangedEventArgs e)
@@ -290,7 +291,7 @@ namespace VAS.UI.Common
 			}
 
 			if (e.PropertyName == "FilterText") {
-				filter.Refilter ();
+				filter?.Refilter ();
 			}
 
 			if (e.PropertyName == "Selection") {
