efc24ebfb6c62cc9e451e56975370589a3878a38
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-11-17 16:10:03 +0100

    Add unit tests for the view models

---
+++ VAS.Tests/Core/ViewModel/TestPlaylistVM.cs
@@ -0,0 +1,85 @@
+﻿//
+//  Copyright (C) 2016 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using NUnit.Framework;
+using VAS.Core.Store.Playlists;
+using VAS.Core.ViewModel;
+using VAS.Core.Store;
+
+namespace VAS.Tests.Core.ViewModel
+{
+	[TestFixture]
+	public class TestPlaylistVM
+	{
+		[Test]
+		public void TestProperties ()
+		{
+			var model = new Playlist {
+				Name = "Playlist",
+				CreationDate = DateTime.Today,
+				LastModified = DateTime.Now,
+			};
+			var viewModel = new PlaylistVM {
+				Model = model,
+			};
+
+			Assert.AreEqual (model.Name, viewModel.Name);
+			Assert.AreEqual (model.CreationDate, viewModel.CreationDate);
+			Assert.AreEqual (model.LastModified, viewModel.LastModified);
+		}
+
+		[Test]
+		public void TestForwardProperties ()
+		{
+			int count = 0;
+
+			var model = new Playlist {
+				Name = "Playlist",
+				CreationDate = DateTime.Today,
+				LastModified = DateTime.Now,
+			};
+			var viewModel = new PlaylistVM {
+				Model = model,
+			};
+
+			viewModel.PropertyChanged += (sender, e) => count++;
+			model.Name = "Test";
+
+			Assert.AreEqual (1, count);
+		}
+
+		[Test]
+		public void TestAddElement ()
+		{
+			int count = 0;
+
+			var model = new Playlist ();
+			var viewModel = new PlaylistVM {
+				Model = model,
+			};
+
+			viewModel.PropertyChanged += (sender, e) => {
+				count++;
+			};
+
+			model.Elements.Add (new PlaylistPlayElement (new TimelineEvent ()));
+			Assert.AreEqual (3, count);
+			Assert.AreEqual (1, viewModel.ViewModels.Count);
+		}
+	}
+}
---
+++ VAS.Tests/Core/ViewModel/TestTemplateVM.cs
@@ -0,0 +1,63 @@
+﻿//
+//  Copyright (C) 2016 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using NUnit.Framework;
+using VAS.Core.Store.Templates;
+using VAS.Core.ViewModel;
+using System.Reflection;
+
+namespace VAS.Tests.Core.ViewModel
+{
+	[TestFixture]
+	public class TestTemplateVM
+	{
+		[Test]
+		public void TestProperties ()
+		{
+			var model = new Utils.DashboardDummy {
+				Name = "dash",
+				Static = true,
+			};
+			var viewModel = new DummyDashboardViewModel {
+				Model = model
+			};
+			model.IsChanged = false;
+
+			Assert.AreEqual (false, viewModel.Edited);
+			Assert.AreEqual ("dash", viewModel.Name);
+			Assert.AreEqual (null, viewModel.Icon);
+		}
+
+		[Test]
+		public void TestEdited ()
+		{
+			var model = new Utils.DashboardDummy {
+				Name = "dash",
+				Static = true,
+			};
+			var viewModel = new DummyDashboardViewModel {
+				Model = model
+			};
+			model.IsChanged = false;
+			Assert.AreEqual (false, viewModel.Edited);
+
+			model.Name = "Test";
+			Assert.AreEqual (true, viewModel.Edited);
+		}
+	}
+}
