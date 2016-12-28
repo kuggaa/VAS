07cef77d84c63a023cfa8b3220cebfbc1b978b89
aameijide <aameijide@fluendo.com>
2016-12-28 17:25:46 +0100

    Add actions to buttons helper

--- VAS.UI.Helpers.Gtk2/ButtonHelper.cs
+++ VAS.UI.Helpers.Gtk2/ButtonHelper.cs
@@ -69,10 +69,22 @@ namespace VAS.UI.Helpers
 		/// <param name="button">Button.</param>
 		/// <param name="icon">Icon.</param>
 		/// <param name="toolTip">Tool tip.</param>
-		/// <param name="command">Command.</param>
-		public static void ApplyStyleNormal (Button button, Pixbuf icon, string toolTip, Command command = null)
+		/// <param name="action">Action.</param>
+		public static void ApplyStyleNormal (Button button, Pixbuf icon, string toolTip, EventHandler action)
 		{
-			Image image = FillButtonWithImage (button, icon, toolTip, command, StyleConf.ButtonNormal);
+			ApplyStyleNormal (button, icon, toolTip, null, action);
+		}
+
+		/// <summary>
+		/// Applies style normal to the given button.
+		/// </summary>
+		/// <param name="button">Button.</param>
+		/// <param name="icon">Icon.</param>
+		/// <param name="toolTip">Tool tip.</param>
+		/// <param name="command">Command.</param>
+		public static void ApplyStyleNormal (Button button, Pixbuf icon, string toolTip, Command command = null, EventHandler action = null)
+		{
+			Image image = FillButtonWithImage (button, icon, toolTip, command, action, StyleConf.ButtonNormal);
 
 			button.WidthRequest = App.Current.Style.ButtonNormalWidth;
 			button.HeightRequest = App.Current.Style.ButtonNormalHeight;
@@ -86,10 +98,22 @@ namespace VAS.UI.Helpers
 		/// <param name="button">Button.</param>
 		/// <param name="icon">Icon.</param>
 		/// <param name="toolTip">Tool tip.</param>
-		/// <param name="command">Command.</param>
-		public static void ApplyStyleTab (Button button, Pixbuf icon, string toolTip, Command command = null)
+		/// <param name="action">Action.</param>
+		public static void ApplyStyleTab (Button button, Pixbuf icon, string toolTip, EventHandler action)
 		{
-			Image image = FillButtonWithImage (button, icon, toolTip, command, StyleConf.ButtonTab);
+			ApplyStyleTab (button, icon, toolTip, null, action);
+		}
+
+		/// <summary>
+		/// Applies style tab to the given button.
+		/// </summary>
+		/// <param name="button">Button.</param>
+		/// <param name="icon">Icon.</param>
+		/// <param name="toolTip">Tool tip.</param>
+		/// <param name="command">Command.</param>
+		public static void ApplyStyleTab (Button button, Pixbuf icon, string toolTip, Command command = null, EventHandler action = null)
+		{
+			Image image = FillButtonWithImage (button, icon, toolTip, command, action, StyleConf.ButtonTab);
 
 			button.WidthRequest = App.Current.Style.ButtonTabWidth;
 			button.HeightRequest = App.Current.Style.ButtonTabHeight;
@@ -102,27 +126,40 @@ namespace VAS.UI.Helpers
 		/// </summary>
 		/// <param name="button">Button.</param>
 		/// <param name="toolTip">Tool tip.</param>
-		/// <param name="command">Command.</param>
-		public static void ApplyStyleDialog (Button button, string toolTip, Command command = null)
+		/// <param name="action">Action.</param>
+		public static void ApplyStyleDialog (Button button, string toolTip, EventHandler action)
 		{
-			FillButton (button, toolTip, command, StyleConf.ButtonDialog);
+			ApplyStyleDialog (button, toolTip, null, action);
+		}
+
+		/// <summary>
+		/// Applies style dialog to the given button.
+		/// </summary>
+		/// <param name="button">Button.</param>
+		/// <param name="toolTip">Tool tip.</param>
+		/// <param name="command">Command.</param>
+		public static void ApplyStyleDialog (Button button, string toolTip, Command command = null, EventHandler action = null)
+		{
+			FillButton (button, toolTip, command, action, StyleConf.ButtonDialog);
 
 			button.HeightRequest = App.Current.Style.ButtonDialogHeight;
 		}
 
-		static void FillButton (Button button, string toolTip, Command command, string buttonStyle)
+		static void FillButton (Button button, string toolTip, Command command, EventHandler action, string buttonStyle)
 		{
 			button.Name = buttonStyle;
 			button.TooltipMarkup = toolTip;
 
 			if (command != null) {
 				button.Bind (command);
+			} else if (action != null) {
+				button.Clicked += action;
 			}
 		}
 
-		static Image FillButtonWithImage (Button button, Pixbuf icon, string toolTip, Command command, string buttonStyle)
+		static Image FillButtonWithImage (Button button, Pixbuf icon, string toolTip, Command command, EventHandler action, string buttonStyle)
 		{
-			FillButton (button, toolTip, command, buttonStyle);
+			FillButton (button, toolTip, command, action, buttonStyle);
 
 			if (icon == null) {
 				return null;
