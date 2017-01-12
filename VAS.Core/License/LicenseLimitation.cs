23e49943011d5631a4bbd778db411098494a9923
aoller <aoller@fluendo.com>
2017-01-12 16:26:33 +0100

    LicenseLimitation changes
    Add LicenseLimitation
    
    Add LicenseLimitationsService
    
    Add CouchbaseStorage Count function using MapReduce
    
    Add ILicenseLimitationsService interface
    
    Refactor ILicenseLimitations
    
    Fix LicenseLimitations
    
    Change LicenseLimitation List to Dictionary
    
    Add LimitedCollectionViewModel
    
    Add publish Storage Events when an object is added or removed
    
    ===============================
    Fixes over aoller's commits:
    
    Remove LicenseLimitation generic parameter from LimitedCollectionViewModel
    
    Remove generics in all the licenseLimitations
    
    Move RALicenseLimitation to VAS
    
    WIP - Make LicenseLimitationsService a real service
    
    WIP - Remove checks for storable
    
    Simplify LimitedCollectionVM
    
    Move LicenseLimitations interface to VAS
    
    Add a BindableViewModelsCollection to NestedViewModel
    
    This property is intended to be always calculated in the child classes, pointing it to the actual collection.
    For now the only different one is the LimitedCollectionViewModel, who points it to the collection holding <max> elements.

---
+++ VAS.Core/License/LicenseLimitation.cs
@@ -0,0 +1,67 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using VAS.Core.MVVMC;
+
+namespace VAS.Core.License
+{
+	/// <summary>
+	/// License limitation.
+	/// This class represent a generic count limitation.
+	/// </summary>
+	public class LicenseLimitation : BindableBase
+	{
+		int maximum;
+
+		/// <summary>
+		/// Gets or sets the name of the limitation.
+		/// </summary>
+		/// <value>The name of the limitation.</value>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the count of licensed items.
+		/// </summary>
+		/// <value>The count.</value>
+		public int Count { get; set; }
+
+		/// <summary>
+		/// Gets or sets the maximum number for the limitation.
+		/// </summary>
+		/// <value>The maximum.</value>
+		public int Maximum {
+			get {
+				// FIXME: Logic in the model?
+				int max = int.MaxValue;
+				if (Enabled) {
+					max = maximum;
+				}
+				return max;
+			}
+			set {
+				maximum = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="T:VAS.Core.License.LicenseLimitation"/> is enabled.
+		/// </summary>
+		/// <value><c>true</c> if enabled; otherwise, <c>false</c>.</value>
+		public bool Enabled { get; set; }
+	}
+}
---
+++ VAS.Services/LicenseLimitationsService.cs
@@ -0,0 +1,108 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using System.Collections.Generic;
+using VAS.Core.Interfaces;
+using VAS.Core.License;
+using VAS.Core.ViewModel;
+
+namespace VAS.Services
+{
+	/// <summary>
+	/// License limitations service.
+	/// </summary>
+	public abstract class LicenseLimitationsService : IService, ILicenseLimitationsService
+	{
+		protected Dictionary<string, LicenseLimitationVM> Limitations;
+
+		public LicenseLimitationsService ()
+		{
+			Limitations = new Dictionary<string, LicenseLimitationVM> ();
+		}
+
+		/// <summary>
+		/// Gets the level of the service. Services are started in ascending level order and stopped in descending level order.
+		/// </summary>
+		/// <value>The level.</value>
+		public int Level {
+			get {
+				return 30;
+			}
+		}
+
+		/// <summary>
+		/// Gets the name of the service
+		/// </summary>
+		/// <value>The name of the service.</value>
+		public string Name {
+			get {
+				return "License limitations";
+			}
+		}
+
+		/// <summary>
+		/// Start the service.
+		/// </summary>
+		public virtual bool Start ()
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Stop the service.
+		/// </summary>
+		public virtual bool Stop ()
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Gets the limitation.
+		/// </summary>
+		/// <returns>The limitation with the specified name, or null.</returns>
+		/// <param name="name">Limitation name.</param>
+		public LicenseLimitationVM Get (string name)
+		{
+			LicenseLimitationVM ret;
+			Limitations.TryGetValue (name, out ret);
+			return ret;
+		}
+
+		/// <summary>
+		/// Gets all the limitations.
+		/// </summary>
+		/// <returns>A collection with all the limitations.</returns>
+		public IEnumerable<LicenseLimitationVM> GetAll ()
+		{
+			return Limitations.Values;
+		}
+
+		/// <summary>
+		/// Add the specified limitation by name.
+		/// </summary>
+		/// <param name="limitation">Limitation.</param>
+		public void Add (LicenseLimitation limitation, Command command = null)
+		{
+			LicenseLimitationVM viewModel = new LicenseLimitationVM {
+				Model = limitation,
+			};
+			// FIXME: Should we overwrite it?
+			Limitations [limitation.Name] = viewModel;
+		}
+	}
+}
