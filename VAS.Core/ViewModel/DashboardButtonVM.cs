032ef58221a99957b99656e5e13401ae7fbf417c
Victor Martos <vmartos@fluendo.com>
2017-01-16 18:28:18 +0100

    Add different buttonVM's and create correctly the instance

--- VAS.Core/ViewModel/DashboardButtonVM.cs
+++ VAS.Core/ViewModel/DashboardButtonVM.cs
@@ -16,6 +16,7 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
+using VAS.Core.Common;
 using VAS.Core.MVVMC;
 using VAS.Core.Store;
 
@@ -26,5 +27,98 @@ namespace VAS.Core.ViewModel
 	/// </summary>
 	public class DashboardButtonVM : ViewModelBase<DashboardButton>
 	{
+		public virtual string View {
+			get;
+		}
+
+		public bool ShowIcon {
+			get {
+				return Model.ShowIcon;
+			}
+			set {
+				Model.ShowIcon = value;
+			}
+		}
+
+		public Color BackgroundColor {
+			get {
+				return Model.BackgroundColor;
+			}
+			set {
+				Model.BackgroundColor = value;
+			}
+		}
+	}
+
+	public class AnalysisEventButtonVM : DashboardButtonVM
+	{
+		AnalysisEventButton model;
+
+		public new AnalysisEventButton Model {
+			get {
+				return (AnalysisEventButton)base.Model;
+			}
+			set {
+				base.Model = value;
+				model = value;
+			}
+		}
+
+		public override string View {
+			get {
+				return "AnalysisEventButtonView";
+			}
+		}
+
+		public TagMode TagMode {
+			get {
+				return model.TagMode;
+			}
+			set {
+				model.TagMode = value;
+			}
+		}
+	}
+
+	public class TagButtonVM : DashboardButtonVM
+	{
+		TagButton model;
+
+		public new TagButton Model {
+			get {
+				return (TagButton)base.Model;
+			}
+			set {
+				base.Model = value;
+				model = value;
+			}
+		}
+
+		public override string View {
+			get {
+				return "TagButtonView";
+			}
+		}
+	}
+
+	public class TimerButtonVM : DashboardButtonVM
+	{
+		TimerButton model;
+
+		public new TimerButton Model {
+			get {
+				return (TimerButton)base.Model;
+			}
+			set {
+				base.Model = value;
+				model = value;
+			}
+		}
+
+		public override string View {
+			get {
+				return "TimerButtonView";
+			}
+		}
 	}
 }
