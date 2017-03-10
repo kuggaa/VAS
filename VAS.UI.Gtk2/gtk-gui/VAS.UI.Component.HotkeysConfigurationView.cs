6c4a714021bc62142f7cb42f060f8825aa50a0fc
Victor Martos <vmartos@fluendo.com>
2017-03-10 14:28:43 +0100

    Refactor HotkeyConfigurationView to match design reqs

--- VAS.UI.Gtk2/gtk-gui/VAS.UI.Component.HotkeysConfigurationView.cs
+++ VAS.UI.Gtk2/gtk-gui/VAS.UI.Component.HotkeysConfigurationView.cs
@@ -6,37 +6,90 @@ namespace VAS.UI.Component
 	{
 		private global::Gtk.VBox vbox2;
 
-		private global::Gtk.ComboBox combobox1;
+		private global::Gtk.Alignment alignment1;
 
-		private global::Gtk.Table table;
+		private global::Gtk.ComboBox categoriesCombo;
+
+		private global::Gtk.HBox hbox1;
+
+		private global::Gtk.Label lblAction;
+
+		private global::Gtk.Label lblShortcut;
+
+		private global::Gtk.ScrolledWindow GtkScrolledWindow;
+
+		private global::Gtk.VBox keyConfigVBox;
 
 		protected virtual void Build ()
 		{
 			global::Stetic.Gui.Initialize (this);
 			// Widget VAS.UI.Component.HotkeysConfigurationView
 			global::Stetic.BinContainer.Attach (this);
+			this.WidthRequest = 450;
+			this.HeightRequest = 600;
 			this.Name = "VAS.UI.Component.HotkeysConfigurationView";
 			// Container child VAS.UI.Component.HotkeysConfigurationView.Gtk.Container+ContainerChild
 			this.vbox2 = new global::Gtk.VBox ();
 			this.vbox2.Name = "vbox2";
 			this.vbox2.Spacing = 6;
 			// Container child vbox2.Gtk.Box+BoxChild
-			this.combobox1 = global::Gtk.ComboBox.NewText ();
-			this.combobox1.Name = "combobox1";
-			this.vbox2.Add (this.combobox1);
-			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.combobox1]));
-			w1.Position = 0;
-			w1.Expand = false;
-			w1.Fill = false;
+			this.alignment1 = new global::Gtk.Alignment (0F, 0.5F, 0F, 1F);
+			this.alignment1.Name = "alignment1";
+			// Container child alignment1.Gtk.Container+ContainerChild
+			this.categoriesCombo = global::Gtk.ComboBox.NewText ();
+			this.categoriesCombo.Name = "categoriesCombo";
+			this.alignment1.Add (this.categoriesCombo);
+			this.vbox2.Add (this.alignment1);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.alignment1]));
+			w2.Position = 0;
+			w2.Expand = false;
+			w2.Fill = false;
 			// Container child vbox2.Gtk.Box+BoxChild
-			this.table = new global::Gtk.Table (((uint)(2)), ((uint)(2)), true);
-			this.table.Name = "table";
-			this.table.RowSpacing = ((uint)(6));
-			this.table.ColumnSpacing = ((uint)(30));
-			this.table.BorderWidth = ((uint)(20));
-			this.vbox2.Add (this.table);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.table]));
-			w2.Position = 1;
+			this.hbox1 = new global::Gtk.HBox ();
+			this.hbox1.Name = "hbox1";
+			this.hbox1.Spacing = 6;
+			// Container child hbox1.Gtk.Box+BoxChild
+			this.lblAction = new global::Gtk.Label ();
+			this.lblAction.WidthRequest = 200;
+			this.lblAction.Name = "lblAction";
+			this.lblAction.Xalign = 0F;
+			this.lblAction.LabelProp = global::Mono.Unix.Catalog.GetString ("Action");
+			this.hbox1.Add (this.lblAction);
+			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.lblAction]));
+			w3.Position = 0;
+			w3.Expand = false;
+			w3.Fill = false;
+			// Container child hbox1.Gtk.Box+BoxChild
+			this.lblShortcut = new global::Gtk.Label ();
+			this.lblShortcut.WidthRequest = 120;
+			this.lblShortcut.Name = "lblShortcut";
+			this.lblShortcut.Xalign = 0F;
+			this.lblShortcut.LabelProp = global::Mono.Unix.Catalog.GetString ("Shortcut");
+			this.hbox1.Add (this.lblShortcut);
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.lblShortcut]));
+			w4.Position = 1;
+			w4.Expand = false;
+			w4.Fill = false;
+			this.vbox2.Add (this.hbox1);
+			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.hbox1]));
+			w5.Position = 1;
+			w5.Expand = false;
+			w5.Fill = false;
+			// Container child vbox2.Gtk.Box+BoxChild
+			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
+			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
+			// Container child GtkScrolledWindow.Gtk.Container+ContainerChild
+			global::Gtk.Viewport w6 = new global::Gtk.Viewport ();
+			w6.ShadowType = ((global::Gtk.ShadowType)(0));
+			// Container child GtkViewport.Gtk.Container+ContainerChild
+			this.keyConfigVBox = new global::Gtk.VBox ();
+			this.keyConfigVBox.Name = "keyConfigVBox";
+			this.keyConfigVBox.Spacing = 6;
+			w6.Add (this.keyConfigVBox);
+			this.GtkScrolledWindow.Add (w6);
+			this.vbox2.Add (this.GtkScrolledWindow);
+			global::Gtk.Box.BoxChild w9 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.GtkScrolledWindow]));
+			w9.Position = 2;
 			this.Add (this.vbox2);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
