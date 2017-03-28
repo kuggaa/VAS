32674f340e5d48c507b4c268af49e768bbc9264c
dfernandez <dfernandez@fluendo.com>
2017-03-28 17:22:25 +0200

    Make virtual the PropertyChangedItem handler

--- VAS.UI.Gtk2/UI/Common/TreeViewBase.cs
+++ VAS.UI.Gtk2/UI/Common/TreeViewBase.cs
@@ -669,12 +669,13 @@ namespace VAS.UI.Common
 			vm.PropertyChanged -= PropertyChangedItem;
 		}
 
-		void PropertyChangedItem (object sender, PropertyChangedEventArgs e)
+		protected virtual void PropertyChangedItem (object sender, PropertyChangedEventArgs e)
 		{
 			var senderVM = sender as IViewModel;
 			if (senderVM == null || Model == null || !dictionaryStore.ContainsKey (senderVM)) {
 				return;
 			}
+
 			TreeIter iter = dictionaryStore [senderVM];
 			store.EmitRowChanged (store.GetPath (iter), iter);
 		}
