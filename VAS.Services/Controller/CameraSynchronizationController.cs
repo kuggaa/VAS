4335c8bb84c55bf67d23ce5f57264d1e8f9f876d
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-04-27 18:20:26 +0200

    Port the cameras synchronization to MVVM

---
+++ VAS.Services/Controller/CameraSynchronizationController.cs
@@ -0,0 +1,187 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using VAS.Core.Common;
+using VAS.Core.Events;
+using VAS.Core.Hotkeys;
+using VAS.Core.Interfaces.MVVMC;
+using VAS.Core.MVVMC;
+using VAS.Core.Store;
+using VAS.Core.ViewModel;
+using VAS.Services.State;
+using VAS.Services.ViewModel;
+
+namespace VAS.Services.Controller
+{
+	[Controller (CameraSynchronizationEditorState.NAME)]
+	public class CameraSynchronizationController : ControllerBase
+	{
+		protected CameraSynchronizationVM cameraSynchronizationVM;
+		protected ProjectVM projectVM;
+		VideoPlayerVM videoPlayerVM;
+
+
+		public override void Start ()
+		{
+			base.Start ();
+			App.Current.EventsBroker.Subscribe<SaveEvent<ProjectVM>> (HandleSave);
+			projectVM.FileSet.PropertyChanged += HandlePropertyChanged;
+			projectVM.Periods.PropertyChanged += HandlePropertyChanged;
+			videoPlayerVM.OpenFileSet (projectVM.FileSet);
+		}
+
+		public override void Stop ()
+		{
+			base.Stop ();
+			App.Current.EventsBroker.Unsubscribe<SaveEvent<ProjectVM>> (HandleSave);
+			projectVM.Periods.PropertyChanged -= HandlePropertyChanged;
+			projectVM.FileSet.PropertyChanged -= HandlePropertyChanged;
+			videoPlayerVM.Pause ();
+		}
+
+		public override void SetViewModel (IViewModel viewModel)
+		{
+			cameraSynchronizationVM = (CameraSynchronizationVM)viewModel;
+			if (viewModel != null) {
+				videoPlayerVM = cameraSynchronizationVM.VideoPlayer;
+				projectVM = cameraSynchronizationVM.Project;
+				InitPeriods ();
+			}
+		}
+
+		public override IEnumerable<KeyAction> GetDefaultKeyActions ()
+		{
+			return new KeyAction [] {
+				// These key actions override the video player ones, so we want to have them highest priority.
+				new KeyAction (
+					App.Current.HotkeysService.GetByName("PLAYER_SEEK_LEFT_SHORT"),
+					() => PerformStep (false), 1
+				),
+				new KeyAction (
+					App.Current.HotkeysService.GetByName("PLAYER_SEEK_RIGHT_SHORT"),
+					() => PerformStep (true), 1
+				),
+			};
+		}
+
+		protected void SaveChanges (SaveEvent<ProjectVM> saveEvent)
+		{
+			if (projectVM != saveEvent.Object) {
+				return;
+			}
+
+			/* If a new camera has been added or a camera has been removed,
+			 * make sure events have a correct camera configuration */
+			foreach (TimelineEventVM evt in projectVM.Timeline.FullTimeline) {
+				int cc = evt.Model.CamerasConfig.Count;
+				int fc = projectVM.FileSet.ViewModels.Count;
+
+				if (cc < fc) {
+					for (int i = cc; i < fc; i++) {
+						evt.Model.CamerasConfig.Add (new CameraConfig (i));
+					}
+				}
+			}
+
+			if (cameraSynchronizationVM.ResynchronizeEvents) {
+				projectVM.Model.ResyncEvents (cameraSynchronizationVM.InitialPeriods);
+			}
+		}
+
+		protected virtual void HandleSave (SaveEvent<ProjectVM> saveEvent)
+		{
+			SaveChanges (saveEvent);
+			App.Current.StateController.MoveBack ();
+		}
+
+		void InitPeriods ()
+		{
+			var gamePeriods = projectVM.Dashboard.GamePeriods;
+
+			MediaFileSetVM fileSet = projectVM.FileSet;
+			var start = new Time (0);
+			var file = fileSet.FirstOrDefault ();
+			var duration = file.Duration;
+			var pDuration = new Time (duration.MSeconds / gamePeriods.Count);
+
+			if (projectVM.Periods == null || projectVM.Periods.ViewModels.Count == 0) {
+				// If no periods are provided create the default ones
+				// defined in the dashboard
+				cameraSynchronizationVM.InitialPeriods = new ObservableCollection<Period> ();
+				foreach (string s in gamePeriods) {
+					Period period = new Period { Name = s };
+					period.Start (start);
+					period.Stop (start + pDuration);
+					cameraSynchronizationVM.InitialPeriods.Add (period);
+					start += pDuration;
+				}
+				projectVM.Periods.Model.Replace (cameraSynchronizationVM.InitialPeriods);
+			} else {
+				// Create a copy of the project periods and keep the
+				// project ones to resynchronize the events in SaveChanges()
+				cameraSynchronizationVM.InitialPeriods = projectVM.Periods.Model.Clone ();
+			}
+		}
+
+		void PerformStep (bool isForward)
+		{
+			MediaFileVM file = projectVM.FileSet.FirstOrDefault (f => f.SelectedGrabber == SelectionPosition.All);
+			if (file != null) {
+				var oneFrame = new Time (1000 / file.Fps);
+				if (isForward) {
+					file.Offset = file.Offset + oneFrame;
+				} else {
+					file.Offset = file.Offset - oneFrame;
+				}
+			} else {
+				if (isForward) {
+					videoPlayerVM.SeekToNextFrame ();
+				} else {
+					videoPlayerVM.SeekToPreviousFrame ();
+				}
+			}
+		}
+
+		void HandlePropertyChanged (object sender, PropertyChangedEventArgs e)
+		{
+			if (sender is TimeNodeVM) {
+				TimeNodeVM timenode = sender as TimeNodeVM;
+				Time seekTime = null;
+
+				if (e.PropertyName == nameof (TimeNode.Start)) {
+					seekTime = timenode.Start;
+				} else if (e.PropertyName == nameof (TimeNode.Stop)) {
+					seekTime = timenode.Stop;
+				}
+				if (seekTime != null) {
+					videoPlayerVM.Pause ();
+					videoPlayerVM.Seek (seekTime);
+				}
+			}
+			if (sender is MediaFileVM) {
+				if (e.PropertyName == nameof (MediaFile.Offset)) {
+					videoPlayerVM.Pause ();
+					videoPlayerVM.Seek (videoPlayerVM.CurrentTime, true);
+				}
+			}
+		}
+	}
+}
---
+++ VAS.Services/State/CameraSynchronizationState.cs
@@ -0,0 +1,66 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using VAS.Core.ViewModel;
+using VAS.Services.ViewModel;
+
+namespace VAS.Services.State
+{
+	public class CameraSynchronizationState : ScreenState<CameraSynchronizationVM>
+	{
+		public const string NAME = "CameraSynchronization";
+
+		public override string Name {
+			get {
+				return NAME;
+			}
+		}
+
+		protected override void CreateViewModel (dynamic data)
+		{
+			ViewModel = new CameraSynchronizationVM ();
+			ViewModel.VideoPlayer = new VideoPlayerVM ();
+			ViewModel.Project = data as ProjectVM;
+			ViewModel.ResynchronizeEvents = true;
+			ViewModel.FixedPeriods = ViewModel.Project.Model.IsFakeCapture;
+		}
+
+		protected override void CreateControllers (dynamic data)
+		{
+			var playerController = new VideoPlayerController ();
+			playerController.SetViewModel (ViewModel.VideoPlayer);
+			Controllers.Add (playerController);
+		}
+
+	}
+
+	public class CameraSynchronizationEditorState : CameraSynchronizationState
+	{
+		new public const string NAME = "CameraSynchronizationEditor";
+
+		public override string Name {
+			get {
+				return NAME;
+			}
+		}
+		protected override void CreateViewModel (dynamic data)
+		{
+			base.CreateViewModel ((object)data);
+			ViewModel.ResynchronizeEvents = false;
+		}
+	}
+}
