65deb4a94f7c0364d3332e2c86218296db136f0e
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-05-13 15:38:44 +0200

    Disable template deletion for non editable templates

---
+++ VAS.Tests/Core/ViewModel/TestTemplateManagerVM.cs
@@ -0,0 +1,60 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using NUnit.Framework;
+using VAS.Core.Store;
+using VAS.Core.Store.Templates;
+using VAS.Core.ViewModel;
+using VAS.Services.ViewModel;
+namespace VAS.Tests.Core.ViewModel
+{
+	[TestFixture]
+	public class TestTemplateManagerVM
+	{
+		TemplatesManagerViewModel<Dashboard, DashboardVM, DashboardButton, DashboardButtonVM> manager;
+
+		[SetUp]
+		public void SetUp ()
+		{
+			manager = new TemplatesManagerViewModel<Dashboard, DashboardVM, DashboardButton, DashboardButtonVM> ();
+		}
+
+		[Test]
+		public void DeleteCommand_LoadedTemplateEditable_Executable ()
+		{
+			manager.LoadedTemplate.Model = new Utils.DashboardDummy ();
+
+			Assert.IsTrue (manager.DeleteCommand.CanExecute ());
+		}
+
+		[Test]
+		public void DeleteCommand_LoadedTemplateNotEditable_NotExecutable ()
+		{
+			manager.LoadedTemplate.Model = new Utils.DashboardDummy ();
+			manager.LoadedTemplate.Model.Static = true;
+
+			Assert.IsFalse (manager.DeleteCommand.CanExecute ());
+		}
+
+		[Test]
+		public void DeleteCommand_LoadedTemplateEmpty_NotExecutable ()
+		{
+			Assert.IsFalse (manager.DeleteCommand.CanExecute ());
+		}
+	}
+}
