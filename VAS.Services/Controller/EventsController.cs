7912d025f0ab0e57a85cd8369c388f294e012dd7
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-05-19 17:22:14 +0200

    Fix seeks when a timer or period changes
    Also cleanup the controller a bit

--- VAS.Services/Controller/EventsController.cs
+++ VAS.Services/Controller/EventsController.cs
@@ -39,47 +39,19 @@ namespace VAS.Services.Controller
 	/// </summary>
 	public class EventsController : ControllerBase
 	{
-		VideoPlayerVM playerVM;
-		TimelineVM timelineVM;
-		ProjectVM projectVM;
-		ICapturerBin capturer;
-
 		/// <summary>
 		/// Gets or sets the current loaded play.
 		/// </summary>
 		/// <value>The loaded play, null if no play.</value>
-		public TimelineEvent LoadedPlay { get; set; }
+		protected TimelineEvent LoadedPlay { get; set; }
 
-		public VideoPlayerVM PlayerVM {
-			get {
-				return playerVM;
-			}
-			set {
-				if (playerVM != null) {
-					playerVM.PropertyChanged -= HandlePlayerVMPropertyChanged;
-				}
-				playerVM = value;
-				if (playerVM != null) {
-					playerVM.PropertyChanged += HandlePlayerVMPropertyChanged;
-				}
-			}
-		}
+		protected virtual VideoPlayerVM VideoPlayer { get; set; }
 
-		public virtual TimelineVM Timeline {
-			get {
-				return timelineVM;
-			}
-			set {
-				if (timelineVM != null) {
-					timelineVM.Filters.PropertyChanged -= HandlePropertyChanged;
-				}
-				timelineVM = value;
-				if (timelineVM != null) {
-					HandleFiltersChanged ();
-					timelineVM.Filters.PropertyChanged += HandlePropertyChanged;
-				}
-			}
-		}
+		protected virtual TimelineVM Timeline { get; set; }
+
+		protected virtual ProjectVM Project { get; set; }
+
+		protected virtual ICapturerBin Capturer { get; set; }
 
 		#region IController implementation
 
@@ -98,6 +70,15 @@ namespace VAS.Services.Controller
 
 			App.Current.EventsBroker.Subscribe<EventLoadedEvent> (HandleEventLoadedEvent);
 			App.Current.EventsBroker.Subscribe<PlaylistElementLoadedEvent> (HandlePlaylistElementLoaded);
+			VideoPlayer.PropertyChanged += HandlePlayerVMPropertyChanged;
+			HandleFiltersChanged ();
+			Timeline.Filters.PropertyChanged += HandlePropertyChanged;
+			if (Project?.Periods != null) {
+				Project.Periods.PropertyChanged += HandlePropertyChanged;
+			}
+			if (Project?.Timers != null) {
+				Project.Timers.PropertyChanged += HandlePropertyChanged;
+			}
 		}
 
 		public override void Stop ()
@@ -115,14 +96,22 @@ namespace VAS.Services.Controller
 
 			App.Current.EventsBroker.Unsubscribe<EventLoadedEvent> (HandleEventLoadedEvent);
 			App.Current.EventsBroker.Unsubscribe<PlaylistElementLoadedEvent> (HandlePlaylistElementLoaded);
+			VideoPlayer.PropertyChanged -= HandlePlayerVMPropertyChanged;
+			Timeline.Filters.PropertyChanged -= HandlePropertyChanged;
+			if (Project?.Periods != null) {
+				Project.Periods.PropertyChanged -= HandlePropertyChanged;
+			}
+			if (Project?.Timers != null) {
+				Project.Timers.PropertyChanged -= HandlePropertyChanged;
+			}
 		}
 
 		public override void SetViewModel (IViewModel viewModel)
 		{
-			PlayerVM = ((IVideoPlayerDealer)viewModel).VideoPlayer;
+			VideoPlayer = ((IVideoPlayerDealer)viewModel).VideoPlayer;
 			Timeline = ((ITimelineDealer)viewModel).Timeline;
-			projectVM = (viewModel as IProjectDealer)?.Project;
-			capturer = (viewModel as ICapturerBinDealer)?.Capturer;
+			Project = (viewModel as IProjectDealer)?.Project;
+			Capturer = (viewModel as ICapturerBinDealer)?.Capturer;
 		}
 
 		public override IEnumerable<KeyAction> GetDefaultKeyActions ()
@@ -132,24 +121,17 @@ namespace VAS.Services.Controller
 
 		#endregion
 
-		protected override void DisposeManagedResources ()
-		{
-			base.DisposeManagedResources ();
-			PlayerVM = null;
-			Timeline = null;
-		}
-
 		protected virtual void HandlePlayerVMPropertyChanged (object sender, PropertyChangedEventArgs e)
 		{
 		}
 
 		void HandleNewEvent (NewEventEvent e)
 		{
-			if (projectVM == null) {
+			if (Project == null) {
 				return;
 			}
-			if (projectVM.IsLive) {
-				if (!capturer.Capturing) {
+			if (Project.IsLive) {
+				if (!Capturer.Capturing) {
 					App.Current.Dialogs.WarningMessage (Catalog.GetString ("Video capture is stopped"));
 					return;
 				}
@@ -159,7 +141,7 @@ namespace VAS.Services.Controller
 				e.Start.ToMSecondsString (), e.Stop.ToMSecondsString (),
 				e.EventType.Name));
 			/* Add the new created play to the project and update the GUI */
-			var play = projectVM.Model.AddEvent (e.EventType, e.Start, e.Stop, e.EventTime, null);
+			var play = Project.Model.AddEvent (e.EventType, e.Start, e.Stop, e.EventTime, null);
 			play.Teams.Replace (e.Teams);
 			if (e.Players != null) {
 				play.Players.Replace (e.Players);
@@ -172,29 +154,29 @@ namespace VAS.Services.Controller
 
 		async void HandleNewDashboardEvent (NewDashboardEvent e)
 		{
-			if (projectVM == null)
+			if (Project == null)
 				return;
 
-			if (projectVM.IsLive) {
-				if (!capturer.Capturing) {
+			if (Project.IsLive) {
+				if (!Capturer.Capturing) {
 					App.Current.Dialogs.WarningMessage (Catalog.GetString ("Video capture is stopped"));
 					return;
 				}
 			}
 
-			if (!projectVM.Model.Dashboard.DisablePopupWindow && e.Edit) {
+			if (!Project.Model.Dashboard.DisablePopupWindow && e.Edit) {
 				e.TimelineEvent.AddDefaultPositions ();
 
 				PlayEventEditionSettings settings = new PlayEventEditionSettings () {
 					EditTags = true, EditNotes = true, EditPlayers = true, EditPositions = true
 				};
 
-				if (projectVM.ProjectType == ProjectType.FileProject) {
-					bool playing = playerVM.Playing;
-					playerVM.Pause ();
+				if (Project.ProjectType == ProjectType.FileProject) {
+					bool playing = VideoPlayer.Playing;
+					VideoPlayer.Pause ();
 					await App.Current.EventsBroker.Publish (new EditEventEvent { TimelineEvent = e.TimelineEvent });
 					if (playing) {
-						playerVM.Play ();
+						VideoPlayer.Play ();
 					}
 				} else {
 					await App.Current.EventsBroker.Publish (new EditEventEvent { TimelineEvent = e.TimelineEvent });
@@ -204,10 +186,11 @@ namespace VAS.Services.Controller
 			Log.Debug (String.Format ("New play created start:{0} stop:{1} category:{2}",
 				e.TimelineEvent.Start.ToMSecondsString (), e.TimelineEvent.Stop.ToMSecondsString (),
 				e.TimelineEvent.EventType.Name));
-			projectVM.Model.AddEvent (e.TimelineEvent);
+			Project.Model.AddEvent (e.TimelineEvent);
 			AddNewPlay (e.TimelineEvent);
-			await App.Current.EventsBroker.Publish (new DashboardEventCreatedEvent { 
-				TimelineEvent = e.TimelineEvent, DashboardButton = e.DashboardButton, DashboardButtons = e.DashboardButtons });
+			await App.Current.EventsBroker.Publish (new DashboardEventCreatedEvent {
+				TimelineEvent = e.TimelineEvent, DashboardButton = e.DashboardButton, DashboardButtons = e.DashboardButtons
+			});
 		}
 
 		void HandleMoveToEventType (MoveToEventTypeEvent e)
@@ -221,18 +204,18 @@ namespace VAS.Services.Controller
 				newEvent.EventType = e.EventType;
 				// Remove all tags from the previous event type but keep global tags
 				newEvent.Tags.RemoveAll (t => (evt.EventType as AnalysisEventType).Tags.Contains (t));
-				projectVM.Model.AddEvent (newEvent);
+				Project.Model.AddEvent (newEvent);
 				App.Current.EventsBroker.Publish (new EventCreatedEvent { TimelineEvent = newEvent });
 			}
 			DeletePlays (newEvents.ToList (), false);
-			Save (projectVM);
+			Save (Project);
 		}
 
 		void HandleDuplicateEvents (DuplicateEventsEvent e)
 		{
 			foreach (var play in e.TimelineEvents) {
 				var copy = play.Clone ();
-				projectVM.Model.AddEvent (copy);
+				Project.Model.AddEvent (copy);
 				App.Current.EventsBroker.Publish (new EventCreatedEvent { TimelineEvent = copy });
 			}
 		}
@@ -244,25 +227,34 @@ namespace VAS.Services.Controller
 
 		void HandleLoadEvent (LoadTimelineEventEvent<TimelineEventVM> e)
 		{
-			PlayerVM.LoadEvent (e.Object?.Model, e.Playing);
+			VideoPlayer.LoadEvent (e.Object?.Model, e.Playing);
 		}
 
 		void HandleLoadEventsList (LoadTimelineEventEvent<IEnumerable<TimelineEventVM>> e)
 		{
-			PlayerVM.LoadEvents (e.Object.Select (vm => vm.Model), e.Playing);
+			VideoPlayer.LoadEvents (e.Object.Select (vm => vm.Model), e.Playing);
 		}
 
 		void HandleLoadEventType (LoadTimelineEventEvent<EventTypeTimelineVM> e)
 		{
 			var timelineEvents = e.Object.ViewModels.Where ((arg) => arg.Visible == true)
 													.Select ((arg) => arg.Model);
-			PlayerVM.LoadEvents (timelineEvents, e.Playing);
+			VideoPlayer.LoadEvents (timelineEvents, e.Playing);
 		}
 
 		void HandlePropertyChanged (object sender, PropertyChangedEventArgs e)
 		{
-			if (e.PropertyName == $"Collection_{nameof (timelineVM.Filters.Elements)}" ||
-				e.PropertyName == nameof (timelineVM.Filters.Active)) {
+			if (sender is TimeNodeVM) {
+				if (e.PropertyName == nameof (TimeNodeVM.Start)) {
+					VideoPlayer.Pause ();
+					VideoPlayer.Seek ((sender as TimeNodeVM).Start, true, false, true);
+				} else if (e.PropertyName == nameof (TimeNodeVM.Stop)) {
+					VideoPlayer.Pause ();
+					VideoPlayer.Seek ((sender as TimeNodeVM).Stop, true, false, true);
+				}
+			}
+			if (e.PropertyName == $"Collection_{nameof (Timeline.Filters.Elements)}" ||
+				e.PropertyName == nameof (Timeline.Filters.Active)) {
 				HandleFiltersChanged ();
 			}
 		}
@@ -278,10 +270,10 @@ namespace VAS.Services.Controller
 		{
 			/* Clip play boundaries */
 			play.Start.MSeconds = Math.Max (0, play.Start.MSeconds);
-			if (projectVM.ProjectType == ProjectType.FileProject) {
-				play.Stop.MSeconds = Math.Min (projectVM.FileSet.Duration.MSeconds, play.Stop.MSeconds);
-				play.CamerasLayout = playerVM.CamerasLayout;
-				play.CamerasConfig = new ObservableCollection<CameraConfig> (playerVM.CamerasConfig);
+			if (Project.ProjectType == ProjectType.FileProject) {
+				play.Stop.MSeconds = Math.Min (Project.FileSet.Duration.MSeconds, play.Stop.MSeconds);
+				play.CamerasLayout = VideoPlayer.CamerasLayout;
+				play.CamerasConfig = new ObservableCollection<CameraConfig> (VideoPlayer.CamerasConfig);
 			} else {
 				play.CamerasLayout = null;
 				play.CamerasConfig = new ObservableCollection<CameraConfig> { new CameraConfig (0) };
@@ -289,15 +281,15 @@ namespace VAS.Services.Controller
 
 			App.Current.EventsBroker.Publish (new EventCreatedEvent { TimelineEvent = play });
 
-			if (projectVM.ProjectType == ProjectType.FileProject) {
-				playerVM.Play ();
+			if (Project.ProjectType == ProjectType.FileProject) {
+				VideoPlayer.Play ();
 			}
-			Save (projectVM);
+			Save (Project);
 
-			if (projectVM.ProjectType == ProjectType.CaptureProject ||
-				projectVM.ProjectType == ProjectType.URICaptureProject) {
+			if (Project.ProjectType == ProjectType.CaptureProject ||
+				Project.ProjectType == ProjectType.URICaptureProject) {
 				if (App.Current.Config.AutoRenderPlaysInLive) {
-					RenderPlay (projectVM.Model, play);
+					RenderPlay (Project.Model, play);
 				}
 			}
 		}
@@ -305,9 +297,9 @@ namespace VAS.Services.Controller
 		void DeletePlays (List<TimelineEvent> plays, bool update = true)
 		{
 			Log.Debug (plays.Count + " plays deleted");
-			projectVM.Timeline.Model.RemoveRange (plays);
-			if (projectVM.ProjectType == ProjectType.FileProject) {
-				Save (projectVM);
+			Project.Timeline.Model.RemoveRange (plays);
+			if (Project.ProjectType == ProjectType.FileProject) {
+				Save (Project);
 			}
 			if (LoadedPlay != null && plays.Contains (LoadedPlay)) {
 				App.Current.EventsBroker.Publish (new LoadEventEvent ());
