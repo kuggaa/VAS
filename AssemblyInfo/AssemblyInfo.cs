6fd80439996145f8c152960898c823c92f9dc534
Sandra Pallarés <sandra@Sandras-MacBook-Pro.local>
2017-05-22 15:39:20 +0200

    Translation moved to longomatch

--- AssemblyInfo/AssemblyInfo.cs
+++ AssemblyInfo/AssemblyInfo.cs
@@ -42,9 +42,9 @@ using System.Runtime.CompilerServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("0.1.0.730")]
-[assembly: AssemblyFileVersion("0.1.0.730-fccb")]
-[assembly: AssemblyInformationalVersion("0.1.0.730-fccb")]
+[assembly: AssemblyVersion("1.3.7.262")]
+[assembly: AssemblyFileVersion("1.3.7.262-28bf-dirty")]
+[assembly: AssemblyInformationalVersion("1.3.7.262-28bf-dirty")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
