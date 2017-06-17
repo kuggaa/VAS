e4bd42aa107a8af65bc972dbc67771401d13b7d0
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-06-17 10:31:42 +0200

    Add a new parameter to draw images with alpha

--- VAS.Core/Interfaces/Drawing/IDrawingToolkit.cs
+++ VAS.Core/Interfaces/Drawing/IDrawingToolkit.cs
@@ -103,9 +103,9 @@ namespace VAS.Core.Interfaces.Drawing
 
 		void DrawText (Point point, double width, double height, string text, bool escape = false, bool ellipsize = false);
 
-		void DrawImage (Image image);
+		void DrawImage (Image image, float alpha = 1);
 
-		void DrawImage (Point start, double width, double height, Image image, ScaleMode mode, bool masked = false);
+		void DrawImage (Point start, double width, double height, Image image, ScaleMode mode, bool masked = false, float alpha = 1);
 
 		void DrawCircleImage (Point center, double radius, Image image);
 
