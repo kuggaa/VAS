6254aec49091cf5cd291e95c3953a84974c8121d
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-06-27 16:40:24 +0200

    Upgrade Fody to 2.x so it works with Mono 5.0

--- Weavers/Helpers/ImplementsInterfaceFinder.cs
+++ Weavers/Helpers/ImplementsInterfaceFinder.cs
@@ -4,53 +4,48 @@ using Mono.Cecil;
 
 public class ImplementsInterfaceFinder
 {
-    TypeResolver typeResolver;
-    Dictionary<string, TypeDefinition> typeReferencesImplementingInterface;
+	TypeResolver typeResolver;
+	Dictionary<string, TypeDefinition> typeReferencesImplementingInterface;
 
-    public ImplementsInterfaceFinder(TypeResolver typeResolver)
-    {
-        this.typeResolver = typeResolver;
-        typeReferencesImplementingInterface = new Dictionary<string, TypeDefinition>();
-    }
+	public ImplementsInterfaceFinder (TypeResolver typeResolver)
+	{
+		this.typeResolver = typeResolver;
+		typeReferencesImplementingInterface = new Dictionary<string, TypeDefinition> ();
+	}
 
-    public TypeDefinition HierarchyImplementsIStorable(TypeReference typeReference)
-    {
-        TypeDefinition baseType;
-        var fullName = typeReference.FullName;
-        if (!typeReferencesImplementingInterface.TryGetValue(fullName, out baseType))
-        {
-            var type = ToDefinition(typeReference);
-            if (ImplementsInterface(type))
-            {
-                baseType = type;
-            }
-            else if (!ShouldSkipBaseType(type))
-            {
-                baseType = HierarchyImplementsIStorable(type.BaseType);
-            }
-        }
+	public TypeDefinition HierarchyImplementsIStorable (TypeReference typeReference)
+	{
+		TypeDefinition baseType;
+		var fullName = typeReference.FullName;
+		if (!typeReferencesImplementingInterface.TryGetValue (fullName, out baseType)) {
+			var type = ToDefinition (typeReference);
+			if (ImplementsInterface (type)) {
+				baseType = type;
+			} else if (!ShouldSkipBaseType (type)) {
+				baseType = HierarchyImplementsIStorable (type.BaseType);
+			}
+		}
 
-        return typeReferencesImplementingInterface[fullName] = baseType;
-    }
+		return typeReferencesImplementingInterface [fullName] = baseType;
+	}
 
-    bool ShouldSkipBaseType(TypeDefinition type)
-    {
-        return type.BaseType == null || (type.BaseType.FullName.StartsWith("System.Collections"));
-    }
+	bool ShouldSkipBaseType (TypeDefinition type)
+	{
+		return type.BaseType == null || (type.BaseType.FullName.StartsWith ("System.Collections"));
+	}
 
-    TypeDefinition ToDefinition(TypeReference typeReference)
-    {
-        if (typeReference.IsDefinition)
-        {
-            return (TypeDefinition) typeReference;
-        }
-        return typeResolver.Resolve(typeReference);
-    }
+	TypeDefinition ToDefinition (TypeReference typeReference)
+	{
+		if (typeReference.IsDefinition) {
+			return (TypeDefinition)typeReference;
+		}
+		return typeResolver.Resolve (typeReference);
+	}
 
-    bool ImplementsInterface(TypeDefinition typeDefinition)
-    {
-        return typeDefinition.Interfaces.Any(x => x.Name == "IStorable");
-    }
+	bool ImplementsInterface (TypeDefinition typeDefinition)
+	{
+		return typeDefinition.Interfaces.Any (x => x.InterfaceType.Name == "IStorable");
+	}
 
 
 }
