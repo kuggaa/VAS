d71d9660508cc8c0435e2162150afa3b4ccd4ddf
Sandra Pallarés <spallares@fluendo.com>
2017-07-06 16:39:34 +0200

    Avoid 2x images failing in analysis view

--- tools/resize-svg.py
+++ tools/resize-svg.py
@@ -53,7 +53,11 @@ def main ():
     files =  subprocess.check_output(["git", "ls-files", di]).split('\n')[:-1]
     files = [x for x in files if x.endswith('.svg') and not "@2x" in x]
     for f in files:
-        if f == "data/icons/hicolor/scalable/actions/vas-missing-video.svg": continue;
+        if f == "data/icons/hicolor/scalable/actions/vas-missing-video.svg" or 
+        f == "icons/hicolor/scalable/actions/ra-tab-event-active" or
+        f == "icons/hicolor/scalable/actions/ra-tab-project-active":
+         continue
+
         resize_svg(f, 2)
 
 if __name__ == '__main__':
