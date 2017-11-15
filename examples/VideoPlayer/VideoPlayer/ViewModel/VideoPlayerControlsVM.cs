1d00658a5fcf81ab9736e7004048dcaa5337209b
Sandra Pallarés <spallares@fluendo.com>
2017-11-15 12:46:04 +0100

    Update skeleton

---
+++ examples/VideoPlayer/VideoPlayer/ViewModel/VideoPlayerControlsVM.cs
@@ -0,0 +1,20 @@
+﻿//
+//  Copyright (C) 2017 ${CopyrightHolder}
+using System;
+using VAS.Core.MVVMC;
+
+namespace VideoPlayer.ViewModel
+{
+	public class VideoPlayerControlsVM
+	{
+		public VideoPlayerControlsVM()
+		{
+		}
+
+		public Command Play { get; set; }
+
+		public Command Next { get; set; }
+
+		public Command Previous { get; set; }
+	}
+}
