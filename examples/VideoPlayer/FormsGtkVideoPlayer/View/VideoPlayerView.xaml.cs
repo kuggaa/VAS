ea78b71fe36608d50c3f189322645f24d509e090
Sandra Pallarés <spallares@fluendo.com>
2017-11-15 10:41:07 +0100

    Add video player view and nuget to filepacker cross platform

---
+++ examples/VideoPlayer/FormsGtkVideoPlayer/View/VideoPlayerView.xaml.cs
@@ -0,0 +1,17 @@
+﻿//
+//  Copyright (C) 2017 ${CopyrightHolder}
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsGtkVideoPlayer.View
+{
+	public partial class VideoPlayerView : ContentPage
+	{
+		public VideoPlayerView()
+		{
+			InitializeComponent();
+		}
+	}
+}
