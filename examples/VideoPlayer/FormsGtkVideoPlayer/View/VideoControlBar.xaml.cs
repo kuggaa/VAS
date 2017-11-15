11c2519d59b90b6b89f8054fdd78a6efa9777ece
Sandra Pallarés <spallares@fluendo.com>
2017-11-15 11:00:26 +0100

    Update view skeleton

---
+++ examples/VideoPlayer/FormsGtkVideoPlayer/View/VideoControlBar.xaml.cs
@@ -0,0 +1,16 @@
+﻿//
+//  Copyright (C) 2017 ${CopyrightHolder}
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FormsGtkVideoPlayer.View
+{
+	public partial class VideoControlBar : ContentView
+	{
+		public VideoControlBar()
+		{
+			InitializeComponent();
+		}
+	}
+}
