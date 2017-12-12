7fcfca82ccd797ef709643c2ce30fc71122e9c16
Dani Garcia Sanchez <dgarcia@fluendo.com>
2017-12-12 16:57:57 +0100

    Add IsNotNullOrWhiteSpaceStringConverter

---
+++ VAS.Core/Common/TypeConverters/IsNotNullOrWhiteSpaceStringConverter.cs
@@ -0,0 +1,22 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace VAS.Core.Common.TypeConverters
+{
+	public class IsNotNullOrWhiteSpaceStringConverter : TypeConverter
+	{
+		public override bool CanConvertTo (ITypeDescriptorContext context, Type destinationType)
+		{
+			return (destinationType == typeof (string));
+		}
+
+		public override object ConvertTo (ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			return !string.IsNullOrWhiteSpace ((string)value);
+		}
+	}
+}
+
