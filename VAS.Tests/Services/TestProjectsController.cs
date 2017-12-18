72f417098155d24f49a747df1764bce9647b0eb7
dfernandez <dfernandez@fluendo.com>
2017-12-18 17:40:05 +0100

    Refactor all files names that contains UI.Ui to just UI
    - Also change wrong copyright holders

--- VAS.Tests/Services/TestProjectsController.cs
+++ VAS.Tests/Services/TestProjectsController.cs
@@ -1,5 +1,5 @@
 ﻿//
-//  Copyright (C) 2017 ${CopyrightHolder}
+//  Copyright (C) 2017 Fluendo S.A.
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
