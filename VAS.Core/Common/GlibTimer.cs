ba0516e05eff169b2cd320391cdbe5a448d8b8e5
Andoni Morales Alastruey <ylatuya@gmail.com>
2018-01-20 10:55:29 +0100

    Add a GLib implementation of ITimer

---
+++ VAS.Core/Common/GlibTimer.cs
@@ -0,0 +1,87 @@
+﻿//
+//  Copyright (C) 2018 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using VAS.Core.Interfaces;
+
+namespace VAS.Core.Common
+{
+	/// <summary>
+	/// Timer wrapper to add an interface
+	/// </summary>
+	public class GlibTimer : ITimer
+	{
+		uint timerID;
+
+		/// <summary>
+		/// Eent raised when the time assigned to the interval has expired
+		/// </summary>
+		public event EventHandler Elapsed;
+
+		/// <summary>
+		/// Gets a value indicating whether timer is running
+		/// </summary>
+		/// <value><c>true</c> if timer running; otherwise, <c>false</c>.</value>
+		public bool Enabled { get => timerID != 0; }
+
+		/// <summary>
+		/// Gets or sets the interval.
+		/// </summary>
+		/// <value>The interval.</value>
+		public double Interval { get; set; }
+
+		/// <summary>
+		/// Start the timer.
+		/// </summary>
+		public void Start ()
+		{
+			timerID = GLib.Timeout.Add ((uint)Interval, HandleIdleHandler);
+		}
+
+		/// <summary>
+		/// Stop the timer.
+		/// </summary>
+		public void Stop ()
+		{
+			GLib.Source.Remove (timerID);
+			timerID = 0;
+		}
+
+		/// <summary>
+		/// Releases all resource used by the <see cref="T:VAS.Core.Interfaces.ITimer"/> object.
+		/// </summary>
+		/// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="T:VAS.Core.Interfaces.ITimer"/>. The
+		/// <see cref="Dispose"/> method leaves the <see cref="T:VAS.Core.Interfaces.ITimer"/> in an unusable state. After
+		/// calling <see cref="Dispose"/>, you must release all references to the <see cref="T:VAS.Core.Interfaces.ITimer"/>
+		/// so the garbage collector can reclaim the memory that the <see cref="T:VAS.Core.Interfaces.ITimer"/> was occupying.</remarks>
+		public void Dispose ()
+		{
+			if (Enabled) {
+				Stop ();
+			}
+		}
+
+		bool HandleIdleHandler ()
+		{
+			if (!Enabled) {
+				return false;
+			}
+			Elapsed?.Invoke (this, new EventArgs ());
+			return true;
+		}
+	}
+}
