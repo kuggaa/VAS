ff91d5634ecee344fe4c73bc718dbe9fe7f0f3fa
Dani Garcia Sanchez <dgarcia@fluendo.com>
2018-01-24 18:30:48 +0100

    Add StateResult

---
+++ VAS.Core/Common/StateResult.cs
@@ -0,0 +1,31 @@
+﻿//
+//  Copyright (C) 2018 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+namespace VAS.Core.Common
+{
+	/// <summary>
+	/// This class is intended to be the results of await a move to an state.
+	/// </summary>
+	public class StateResult<T>
+	{
+		/// <summary>
+		/// Gets or sets the result.
+		/// </summary>
+		/// <value>The result.</value>
+		public T Result { get; set; }
+	}
+}
