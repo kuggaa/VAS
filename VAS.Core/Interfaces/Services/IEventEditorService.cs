f865f6cc50003da0cec127dfa8615573e3e426ed
Víctor Guzmán <vguzman@fluendo.com>
2018-02-13 17:49:11 +0100

    Migrate EditEventEvent to a service

---
+++ VAS.Core/Interfaces/Services/IEventEditorService.cs
@@ -0,0 +1,12 @@
+﻿//
+//  Copyright (C) 2018 Fluendo S.A.
+using System.Threading.Tasks;
+using VAS.Core.ViewModel;
+
+namespace VAS.Core.Interfaces.Services
+{
+	public interface IEventEditorService
+	{
+		Task EditEvent (TimelineEventVM timelineEvent);
+	}
+}
