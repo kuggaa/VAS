d331aed16565ab54c477f026b6bcc94fda3006af
Victor Martos <vmartos@fluendo.com>
2018-02-21 17:30:29 +0100

    Create vasone solution for all projects and change makefiles
    - add .gitignore
    - Add the vasone solution
    - Add paket to root
    - Add RiftDebug, RiftRelease, LongoRelease and LongoDebug Solution Configurations
    - Move tools to root Repository and make changes for paths
    - Create makefiles for vasone

--- build/common.mk
+++ build/common.mk
@@ -8,6 +8,7 @@ INSTALL_DIR = cp -r
 XBUILD=xbuild
 MONO=mono
 NUNIT_CONSOLE_EXE=packages/NUnit.ConsoleRunner/tools/nunit3-console.exe
+NUNIT = $(MONO) --debug $(NUNIT_CONSOLE_EXE)
 
 prefix ?= /usr/local
 datarootdir = ${prefix}/share
@@ -23,9 +24,27 @@ datadir = ${datarootdir}/${USER_PACKAGE}
 
 MAKE_ENVS=LIBDIR=$(DESTDIR)$(exelibdir) \
 	OUTPUT_DIR=$(OUTPUT_DIR) \
-	PACKAGE=$(PACKAGE) \
 	OSTYPE=$(OSTYPE) \
 	WITH_WIBU=$(WITH_WIBU) \
 	DEVEL=$(DEVEL) \
 	CPU=$(CPU) \
 	prefix=$(prefix)
+
+define build
+$(XBUILD) /property:DefineConstants="$(XBUILD_CONSTANTS)" \
+  /property:BuildType=makefiles \
+  /property:BuildProjectReferences=false \
+  /property:Platform="Any CPU" \
+  /property:Configuration=$(2) $(1)
+endef
+
+define install
+$(MKDIR_P) $(DESTDIR)$(2)
+$(INSTALL) $(wildcard $(1)/bin/*.dll) $(DESTDIR)$(2)
+$(INSTALL) $(wildcard $(1)/bin/*.pdb) $(DESTDIR)$(2)
+$(INSTALL) $(wildcard $(1)/bin/*.dll.config) $(DESTDIR)$(2)
+$(MKDIR_P) $(DESTDIR)$(3)
+$(INSTALL_DIR) $(1)/data/theme $(DESTDIR)$(3)
+$(INSTALL_DIR) $(1)/data/icons $(DESTDIR)$(3)
+$(INSTALL_DIR) $(1)/data/images $(DESTDIR)$(3)
+endef
--- tools/ide_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CERBERO_PREFIX=~/oneplay-build/dist/linux_x86_64/
-export PATH=$PATH:~/oneplay-build/build-tools-linux-x86_64/bin/
-export PKG_CONFIG_PATH=$CERBERO_PREFIX/lib/pkgconfig/
-export MONO_PATH=$CERBERO_PREFIX/lib/mono/4.5/:$CERBERO_PREFIX/lib/mono/4.5/Facades
-export LD_LIBRARY_PATH=$CERBERO_PREFIX/lib
-monodevelop $@
-
