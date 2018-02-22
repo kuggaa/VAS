c01abfeef07d935f2c62e298a19e03a227a5dbec
Víctor Guzmán <vguzman@fluendo.com>
2018-02-22 18:09:32 +0100

    Set default callbacks in the service itself

--- VAS.Core/Interfaces/Services/IEventEditorService.cs
+++ VAS.Core/Interfaces/Services/IEventEditorService.cs
@@ -1,12 +1,15 @@
 ﻿//
 //  Copyright (C) 2018 Fluendo S.A.
 using System.Threading.Tasks;
+using VAS.Core.Interfaces.MVVMC;
 using VAS.Core.ViewModel;
 
 namespace VAS.Core.Interfaces.Services
 {
-	public interface IEventEditorService
+	public interface IEventEditorService : IController
 	{
 		Task EditEvent (TimelineEventVM timelineEvent);
+
+		void SetDefaultCallbacks (TimelineVM timeline);
 	}
 }
