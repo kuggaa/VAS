4b116784b0207b11edffd423ec6c89fe1bba7732
Victor Martos <vmartos@fluendo.com>
2018-02-22 12:46:35 +0100

    Move tools to root Repository and make changes for paths

--- tools/runtools_vas.py
+++ tools/runtools_vas.py
@@ -4,26 +4,28 @@ import sys
 import subprocess
 import shlex
 
-path = os.path.join(os.path.dirname(__file__), '..', '..', 'tools')
-sys.path.append(path)
-
 # import all tools
-import update_data
 import resize_svg
-import runtools
+import generate_mobile_icons
+import update_mobile_resources
+import update_palette_android
+import update_palette_gtkrc
 
-application_tools = [runtools]
-# update_data should be the last one executed, as it depends on everything being already generated
-desktop_tools = [resize_svg]
+mobile_tools = [generate_mobile_icons, update_mobile_resources, update_palette_android]
+desktop_tools = [resize_svg, update_palette_gtkrc]
 
 def main():
-    tools = application_tools + desktop_tools
+    tools = mobile_tools + desktop_tools
     if not os.getenv('CERBERO_PREFIX'):
         raise ValueError("This script should be run inside a cerbero shell")
     print "===== Running tools %s =====" % [t.__name__ for t in tools]
     for tool in tools:
         print "===== Running %s =====" % tool.__name__
-        tool.main()
+        if tool.__name__ == "resize_svg":
+            tool.main('riftanalyst')
+            tool.main('longomatch')
+        else:
+            tool.main()
     print "===== Finished running tools ====="
     git_status = subprocess.check_output(shlex.split("git status --porcelain --ignore-submodules"))
     git_status = git_status.replace('M longomatch.desktop.in', '').replace('M AssemblyInfo/AssemblyInfo.cs', '')
