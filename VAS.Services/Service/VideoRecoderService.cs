8b16f5cebd8501c7346e9eb6d10f913367554876
Víctor Guzmán <vguzman@fluendo.com>
2018-02-23 12:28:24 +0100

    Create a VideoRecorderService. It will be completed with the one in RA-1294

---
+++ VAS.Services/Service/VideoRecoderService.cs
@@ -0,0 +1,35 @@
+﻿//
+//  Copyright (C) 2018 Fluendo S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VAS.Core;
+using VAS.Core.Common;
+using VAS.Core.Events;
+using VAS.Core.Interfaces;
+using VAS.Core.MVVMC;
+using VAS.Core.Store;
+using VAS.Core.ViewModel;
+
+namespace VAS.Services.Service
+{
+	public class VideoRecoderService : DisposableBase, IVideoRecorderService
+	{
+
+	}
+}
